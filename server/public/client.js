d87587d67eecc30958eca7e3e92ee33d011032fd
Robyn Noble <robynhurd09@gmail.com>
2021-04-16 08:49:26 -0500

    first few functions written

--- server/public/client.js
+++ server/public/client.js
@@ -5,3 +5,25 @@ $(onReady);
 function onReady() {
     console.log('Hi from jQuery');
 };
+
+function firstValue(){
+    let valueOne = {
+        integer: $( '#first-inputs').val(),
+    }
+    console.log('First value:', valueOne);
+    $.ajax({
+        method: 'POST',
+        url: '/valone',
+        data: valueOne,
+    })//end ajax
+    .then  (function (response){
+        console.log('Value one');
+        getOne();
+    })
+    .catch(function(error) {
+        console.log('Error from server', error);
+        alert('Sorry, could not retrieve input, try again');
+    })
+    $('#first-inputs');
+}//end firstValue
+
